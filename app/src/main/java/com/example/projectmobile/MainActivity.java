d53ca53c6fb75dd1b48a0e6e70456da24267ecc8
ste-corizzato <stefano.corizzato@studenti.unimi.it>
2019-12-03 14:22:26 +0100

    ho aggiunto i pulsanti: play, leaderboards e impostazioni

--- app/src/main/java/com/example/projectmobile/MainActivity.java
+++ app/src/main/java/com/example/projectmobile/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.projectmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
